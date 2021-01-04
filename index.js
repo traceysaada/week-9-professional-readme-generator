3e7020368a819e3aa70ef00f329c4c2575ca6968
Tracey Saada <tracey.saada@outlook.com>
2021-01-04 18:39:31 +0000

    inital setup of readme questions

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
-
-]
+  {
+    type: "input",
+    name: "title",
+    message: "what is the title of this application",
+  },
+];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+  });
 }
 
 // function call to initialize program
