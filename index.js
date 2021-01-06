165f7cb7af7e535a15f1f8d37413f30ae32418eb
Tracey Saada <tracey.saada@outlook.com>
2021-01-06 22:19:26 +0000

    added write to file & directory

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const path = require('Path');
 const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
@@ -8,17 +9,68 @@ const questions = [
     name: "title",
     message: "what is the title of this application",
   },
+  {
+    type: "input",
+    name: "description",
+    message: "what will this application do ",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "what are the instructions for this installation",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "how can a user benefit from this application",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "how did you make this application function",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "how do you use this appliction",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+// we are up to the point where we are passing in the name of the file we want to create 
+  //and the data we want to use look into how to write data to a file
+  // work out how we can pass our object to the 'generateMarkdow' 
+function writeToFile(title, description, installation, usage, contributing, test); {
+  fs.writeToFile("response.txt", generateMarkdown(response)), err => {
+    err ? console.error (err) : console.log("success!");
+  } 
+
+// look into the 'path' module to see how we can get the current working directory and tell the
+  // program where to create our new file
+  mkdirRecurse = function (inputPath) {
+    if (fs.existsSync(inputPath)) {
+      return;
+    }
+    const basePath = path.dirname(inputPath);
+    if (fs.existsSync(basePath)) {
+      fs.mkdirSync(inputPath);
+    }
+    mkdirRecurse(basePath);
+  }
+};
+
+  //function and get back a string to write to the created file
+
+  
+
 
 // function to initialize program
 function init() {
   inquirer.prompt(questions).then((answers) => {
     console.log(answers);
+    writeToFile("generatedReadme.md", answers);
   });
-}
+};
 
 // function call to initialize program
 init();
